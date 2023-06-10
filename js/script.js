01bad022f7330285576494f65d23b5662a9feed0
Gian Fava <49363295+gianfava@users.noreply.github.com>
2023-06-10 16:30:46 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+
+
+
+function AbrirEspecialidades() {
+  window.open("especialidades.html");
+}
+
+function AbrirPlanos() {
+  window.open("planos.html");
+}
+
+function AbrirUsuario() {
+  window.open("logado.html");
+}
+
+
